ebb360f604ef08be63429a71a23dd93230244a7e
Sanaa Syed <sanaasyed@dhcp-84-34.cu-internet.carleton.ca>
2020-01-14 20:26:22 -0500

    add a query to find the top 5 most used tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
   def stats
     @user_with_most_c = (Post.all).group(:user_id).first.user.first_name
     @most_c = (Post.all).group(:user_id).count().first[1]
+    @most_used_tags = Tagging.joins(:tag).group(:tag_name).count().first[0]
   end 
 
   # POST /posts
